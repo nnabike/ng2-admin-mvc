c25052eb260b01e18ffb9f250b69269a37cfcacd
samer.alrawi <samer_alias@outlook.com>
2017-08-17 22:53:06 -0400

    brought ng components

---
+++ ScrollerMedia/src/app/theme/components/baFullCalendar/baFullCalendar.component.ts
@@ -0,0 +1,23 @@
+import {Component, ViewChild, Input, Output, ElementRef, EventEmitter} from '@angular/core';
+import 'fullcalendar/dist/fullcalendar.js';
+import * as jQuery from 'jquery';
+import * as JQuery from '@types/fullcalendar';
+
+
+@Component({
+  selector: 'ba-full-calendar',
+  templateUrl: './baFullCalendar.html'
+})
+export class BaFullCalendar {
+
+  @Input() baFullCalendarConfiguration:Object;
+  @Input() baFullCalendarClass:string;
+  @Output() onCalendarReady = new EventEmitter<any>();
+
+  @ViewChild('baFullCalendar') public _selector:ElementRef;
+
+  ngAfterViewInit() {
+    let calendar = jQuery(this._selector.nativeElement).fullCalendar(this.baFullCalendarConfiguration);
+    this.onCalendarReady.emit(calendar);
+  }
+}
